348b0df2e32a925f47228dd8eccf3eb6e7b1ae65
ik013043z1 <ik013043z1@hz20607.H013043.NET>
2018-10-22 09:38:14 +0200

    case 6 - binario. da fallo

--- src/primero/Menu.java
+++ src/primero/Menu.java
@@ -80,6 +80,34 @@ public class Menu {
 			}
 			break;
 		case 6:
+			long numero;
+			long aux;
+			long digito;
+			long decimal;
+			int exponente;
+			boolean esBinario;
+			
+			do {
+			System.out.println("Introduce un numero binario");	
+			esBinario=true;
+			aux=numero;
+			while (aux!=0) {
+				digito=aux%10;
+				if(digito!=0 && digito !=1) {
+					esBinario=false;
+				}
+				aux=aux/10;
+			}
+			}while (!esBinario);
+			exponente=0;
+			decimal=0;
+			while (numero!=0) {
+				digito=numero%10;
+				decimal=decimal+digito*(int)Math.pow(2, exponente);
+				exponente++;
+				numero=numero/10;
+			}
+			System.out.println("Decimal: "+decimal);
 			break;
 		case 7:
 			System.exit(0);

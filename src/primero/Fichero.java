3f9d15fe9f5fccb1c960798f2eac9f81b614ed4f
ik013043z1 <ik013043z1@hz20607.H013043.NET>
2018-11-16 13:58:18 +0100

    qwerifd

--- src/primero/Fichero.java
+++ src/primero/Fichero.java
@@ -1,9 +1,25 @@
 package primero;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Fichero {
 
 	public static void main(String[] args) {
-		//investigar como leer y escribir en fichero
+		// investigar como leer y escribir en fichero
+
+		try {
+			File f = new File("fichero1.txt");
+			Scanner s = new Scanner(f);
+			while (s.hasNextLine()) {
+				String line = s.nextLine();
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Ell archivo no existe");
+
+		}
 	}
 
 }
